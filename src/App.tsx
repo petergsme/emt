fed6145a62ab6b7cce764761b889f247b6912655
petergsme <garciasan@esat-alumni.com>
2025-05-02 17:41:25 +0200

    prueba

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 
 function Home() {
-  return <h2>Inicio</h2>;
+  return <h2>tengo ganas de fiesta</h2>;
 }
 
 function About() {
-  return <h2>Acerca</h2>;
+  return <h2>hasta luego</h2>;
 }
 
 function App() {
   return (
     <div>
       <nav>
-        <Link to="/">Inicio</Link> | <Link to="/about">Acerca</Link>
+        <Link to="/">no puede ser</Link> | <Link to="/about">que hay</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
