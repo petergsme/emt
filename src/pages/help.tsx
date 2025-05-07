d293a12949e59ec6eb2bbe17c6f025b47b83ed9c
Irene <irenecallei@gmail.com>
2025-05-07 20:07:38 +0200

    feat: Add pages

---
+++ src/pages/help.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Help = () => {
+  return <div>Ayuda</div>;
+};
+
+export default Help;
---
+++ src/pages/titulos-y-tarifas.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TitulosYTarifas = () => {
+  return <div>TitulosYTarifas</div>;
+};
+
+export default TitulosYTarifas;
