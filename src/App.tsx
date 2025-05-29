ee399090ea3f5a14ddf8a12a4d4f4047f8531064
Pedro Garcia <garciasan@esat-alumni.com>
2025-05-29 13:21:26 +0200

    reacti18next package fully configured, folder structure created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from "./pages/home";
-import NavbarMobile from "./components/mobile/NavbarMobile";
-import "./App.module.scss";
-import "@/styles/mixins.scss";
+import Home from './pages/home';
+import NavbarMobile from './components/mobile/NavbarMobile';
+import './App.module.scss';
+import '@/styles/mixins.scss';
+import './i18n';
 
 const App: React.FC = () => {
   return (
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,33 +1,25 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
-const resources = {
-  en: {
-    translation: {
-      'Welcome to React': 'Welcome to React and react-i18next',
-    },
-  },
-  fr: {
-    translation: {
-      'Welcome to React': 'Bienvenue à React et react-i18next',
-    },
-  },
-};
+import HttpApi from 'i18next-http-backend';
 
 i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(HttpApi)
+  .use(initReactI18next)
   .init({
-    resources,
-    lng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
+    lng: 'es',
+    fallbackLng: 'es',
+    debug: true,
 
     interpolation: {
-      escapeValue: false, // react already safes from xss
+      escapeValue: false,
     },
+
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+
+    ns: ['common'], //If you add more namespaces include them here.
+    defaultNS: 'common',
   });
 
 export default i18n;
