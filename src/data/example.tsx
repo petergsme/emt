52ed177b03ee3a44c2e325878a73249ef4b8d7b3
petergsme <garciasan@esat-alumni.com>
2025-05-30 19:08:12 +0200

    created data folder to store mobilis cards data objects

---
+++ src/data/example.tsx
@@ -0,0 +1,27 @@
+// import { BusCardInfo } from "../types/BusCardInfo";
+
+// export const busCardsData: BusCardInfo[] = [
+//   {
+//     name: "Metro Bus Pass",
+//     primaryDescription: "Monthly unlimited bus pass for city transit",
+//     tags: ["monthly", "unlimited", "city"],
+//     secondaryDescription: "Valid for all city bus routes within metropolitan area",
+//     hasButton: true,
+//     buttonText: "Purchase Online",
+//     buttonURL: "https://metro.city/purchase",
+//     additionalInfo: ["Valid 30 days from activation", "Non-transferable"],
+//     price: "$85",
+//     hasRequiredDocuments: true,
+//     requiredDocuments: ["Photo ID", "Proof of residence"],
+//     hasForm: true,
+//     formURL: "https://metro.city/application",
+//     hasRules: true,
+//     rules: ["Must be activated within 90 days", "No refunds after activation"],
+//     hasRegulations: true,
+//     RegulationsURL: "https://metro.city/regulations",
+//     hasAppointmentButton: false,
+//   },
+//   // ... 7 more cards
+// ];
+
+//Commented because model/interface/type doesn't exist.
