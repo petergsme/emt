5bf76f8647c4ebdf0cf60783647f124239c739d1
Pedro Garcia <garciasan@esat-alumni.com>
2025-06-01 18:27:25 +0200

    added classnames package, updated all packages & deleted deprecated icons and react-icons packages

--- src/assets/icons/Icon.tsx
+++ src/assets/icons/Icon.tsx
@@ -1,19 +1,27 @@
 import { createElement } from 'react';
 import { icons } from './icons';
+import classNames from 'classnames/bind';
 import theme from './icon.module.scss';
 
 export type IconName = keyof typeof icons;
 
+const cx = classNames.bind(theme);
 interface IconProps {
   icon: IconName;
   size?: 'small' | 'medium' | 'large';
   color?: 'onprimary' | 'onprimary-secondary' | 'onbrand' | 'onbrand-secondary' | 'disabled';
 }
 
+//Using classnames package as an example, even if here it doesn't change much, but at least classes look more organized and I don't need to write theme.whateverclass all the time.
+
 export const Icon = ({ icon, size = 'medium', color = 'onprimary' }: IconProps) => {
   return (
     <span
-      className={`${theme.icon} ${theme[`icon__size--${size}`]} ${theme[`icon__color--${color}`]}`}
+      className={cx({
+        icon: true,
+        [`icon__size--${size}`]: true,
+        [`icon__color--${color}`]: true,
+      })}
       aria-label={`${icon}-icon`}
       role="img"
     >
@@ -21,3 +29,17 @@ export const Icon = ({ icon, size = 'medium', color = 'onprimary' }: IconProps) 
     </span>
   );
 };
+
+//This is the simple way of doing it, no classname package involved. A bit more difficult to read.
+
+// export const Icon = ({ icon, size = 'medium', color = 'onprimary' }: IconProps) => {
+//   return (
+//     <span
+//       className={`${theme.icon} ${theme[`icon__size--${size}`]} ${theme[`icon__color--${color}`]}`}
+//       aria-label={`${icon}-icon`}
+//       role="img"
+//     >
+//       {createElement(icons[icon], {})}
+//     </span>
+//   );
+// };
--- src/components/mobile/NavbarMobile.tsx
+++ src/components/mobile/NavbarMobile.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import { FaBars } from "react-icons/fa";
-import SidebarMobile from "./SidebarMobile";
-import "./NavbarMobile.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import SidebarMobile from './SidebarMobile';
+import './NavbarMobile.css';
 
 const NavbarMobile: React.FC = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -16,21 +15,11 @@ const NavbarMobile: React.FC = () => {
     <>
       <nav className="nav-items">
         <Link to="/" className="logo">
-          <img
-            src="/emt.png"
-            alt="EMT Logo"
-            className="logo-img"
-            style={{ height: "20px" }}
-          />
+          <img src="/emt.png" alt="EMT Logo" className="logo-img" style={{ height: '20px' }} />
         </Link>
-        <button onClick={toggleSidebar} className="menu-toggle">
-          <FaBars />
-        </button>
+        <button onClick={toggleSidebar} className="menu-toggle"></button>
       </nav>
-      <SidebarMobile
-        isOpen={isSidebarOpen}
-        onClose={() => setIsSidebarOpen(false)}
-      />
+      <SidebarMobile isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
     </>
   );
 };
