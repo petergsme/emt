02d472b42fcc1a746936b35172db94056154818f
Pedro Garcia <garciasan@esat-alumni.com>
2025-06-08 21:23:19 +0200

    working on RegisterForm modal usage in Homepage

--- src/components/forms/RegisterForm/RegisterForm.tsx
+++ src/components/forms/RegisterForm/RegisterForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { RegisterFormData } from '../RegisterFormData';
 import { FormInput } from '../FormInput/FormInput';
@@ -7,14 +8,15 @@ import { FormModal } from '../FormModal/FormModal';
 import { Icon } from '@/assets/icons/Icon';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-// import classNames from 'classnames/bind';
 import theme from './RegisterForm.module.scss';
-import { useState } from 'react';
 
-// const cx = classNames.bind(theme);
+interface FormProps {
+  setIsOpenSection: (state: boolean) => void;
+}
 
-export const RegisterForm = () => {
-  const [isOpen, setIsOpen] = useState(false);
+export const RegisterForm = ({ setIsOpenSection }: FormProps) => {
+  const [isOpenExit, setIsOpenExit] = useState(false);
+  const [isOpenSuccess, setIsOpenSuccess] = useState(false);
 
   const { t } = useTranslation('form');
 
@@ -22,7 +24,7 @@ export const RegisterForm = () => {
     register,
     control,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
     watch,
   } = useForm<RegisterFormData>();
 
@@ -33,7 +35,7 @@ export const RegisterForm = () => {
   return (
     <>
       <section className={theme.register__section}>
-        <button type="button" onClick={() => setIsOpen(true)} className={theme['button-openModal']}>
+        <button type="button" onClick={() => setIsOpenExit(true)} className={theme['button-openModal']}>
           <Icon icon="Close" size="large" color="onprimary-secondary" />
         </button>
 
@@ -72,39 +74,40 @@ export const RegisterForm = () => {
             <button
               type="submit"
               className={`label-button ${theme['submit-button-test']}`}
-              onClick={() => setIsOpen(true)}
+              onClick={() => {
+                if (isValid) setIsOpenSuccess(true);
+              }}
             >
               {t('sectionText.register.button')}
             </button>
           </div>
         </form>
       </section>
-      {/* {isOpen && (
+
+      {isOpenExit && (
         <FormModal>
-          <h2 className="display-medium center-text text__color--brand">¿Seguro?</h2>
-          <p className="paragraph-small">
-            Si abandonas el proceso perderás los datos que has introducido y tendrás que volver a empezar.
-          </p>
+          <h2 className="display-medium center-text text__color--brand">{t('modal.abandon.claim')}</h2>
+          <p className="paragraph-small">{t('modal.abandon.info')}</p>
           <div className={theme['flex-utility']}>
-            <button className={`label-button ${theme['submit-button-test']}`} onClick={() => setIsOpen(false)}>
-              NO
+            <button className={`label-button ${theme['submit-button-test']}`} onClick={() => setIsOpenExit(false)}>
+              {t('modal.abandon.continue')}
             </button>
             <button
               className={`label-button ${theme.blanco} ${theme['submit-button-test']}`}
-              onClick={() => setIsOpen(false)}
+              onClick={() => setIsOpenSection(false)}
             >
-              SÍ
+              {t('modal.abandon.exit')}
             </button>
           </div>
         </FormModal>
-      )} */}
+      )}
 
-      {isOpen && (
+      {isOpenSuccess && (
         <FormModal>
-          <h2 className="display-medium center-text text__color--brand">Registro Completado</h2>
-          <p className="paragraph-small">Enhorabuena, ahora tus viajes están a buen recaudo.</p>
-          <button className={`label-button ${theme['submit-button-test']}`} onClick={() => setIsOpen(false)}>
-            VOLVER ATRÁS
+          <h2 className="display-medium center-text text__color--brand">{t('modal.completed.register.claim')}</h2>
+          <p className="paragraph-small">{t('modal.completed.register.info')}</p>
+          <button className={`label-button ${theme['submit-button-test']}`} onClick={() => setIsOpenSection(false)}>
+            {t('modal.completed.exit')}
           </button>
         </FormModal>
       )}
@@ -112,12 +115,6 @@ export const RegisterForm = () => {
   );
 };
 
-// anida los textos generales de form, cambia el nombre y replace a todos los usos del t con ese nombre de namespace, tambien lo otro.
+//  deberas meterle css a este componente en si mismo o su wrapper en el componente padre, para convertirlo en un fixed que aparece encima o en un lateral o como sea.
 
-//  luego apañamos las traducciones. luego apañamos la logica que hace abrir uno u otro.
-
-// hace falta otro modal para lo que viene siendo el de compleción. recuerda hacer funcionar esas funciones bien. y luego deberas meterle css a este componente en si mismo o su wrapper en el componente padre, para convertirlo en un fixed que aparece encima o en un lateral o como sea.
-// pasarle (tipada) la propiedad que tendra que recibir el boton ese de la cruz para cerrar el popup.
-// crear el custom hook ver si puede tener tambien imports rollo forminput tal cual, creo que eso no, es verdad eso no. simplemente mueve logica.
-
-// dos opciones para que el boton abra lo que toca --> que la validacion dependa de que el formulario se ha enviado (guardar en una variable el valor y si existe se pinta el componente que toca.) o nuevo usestate para eso. o mismo usestate pero uso de useref como antes para saber si el click fue en el boton de cerrar o en el boton de enviar y segun sea uno u otro abrir un modal u otro.
+// crear el custom hook mover toda la logica.
