ad674ce4fe6de78ed837a1c07a50e208d6a7526b
Pedro Garcia <garciasan@esat-alumni.com>
2025-06-12 17:50:39 +0200

    card component created

---
+++ src/components/common/Card/Card.tsx
@@ -0,0 +1,13 @@
+import classNames from 'classnames';
+import theme from './card.module.scss';
+
+const cx = classNames.bind(theme);
+
+interface CardProps {
+  children: React.ReactNode;
+  isFullWidth?: boolean;
+}
+
+export const Card = ({ children, isFullWidth = false }: CardProps) => {
+  return <article className={cx('card', { 'card--fullWidth': isFullWidth })}>{children}</article>;
+};
