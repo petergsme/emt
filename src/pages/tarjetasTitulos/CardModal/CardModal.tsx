0210d0d80a662791532f14395ffac3852f2b17a7
Pedro Garcia <garciasan@esat-alumni.com>
2025-06-14 13:09:34 +0200

    fixed scroll issues on iOS

--- src/pages/tarjetasTitulos/CardModal/CardModal.tsx
+++ src/pages/tarjetasTitulos/CardModal/CardModal.tsx
@@ -14,11 +14,19 @@ interface FormModalProps {
 export const CardModal = ({ children, isClosing, onClose }: FormModalProps) => {
   useEffect(() => {
     document.body.style.overflow = 'hidden';
-    document.body.style.touchAction = 'manipulation';
+
+    const scrollY = window.scrollY;
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${scrollY}px`;
+    document.body.style.width = '100%';
 
     return () => {
       document.body.style.overflow = 'unset';
-      document.body.style.touchAction = 'auto';
+
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.width = '';
+      window.scrollTo(0, scrollY);
     };
   }, []);
 
