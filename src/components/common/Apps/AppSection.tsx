4b79181e633854d809ab086f29211c5a847a80d7
Pedro Garcia <garciasan@esat-alumni.com>
2025-06-16 23:44:55 +0200

    finished app section component & implemented it

--- src/components/common/Apps/AppSection.tsx
+++ src/components/common/Apps/AppSection.tsx
@@ -1,18 +1,23 @@
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import cn from 'classnames';
 import theme from './appSection.module.scss';
 import { AppCard } from './appCard/appCard';
-// import Swiper from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import emticket_app_icon from '@/assets/emticket_app_icon.webp';
 import emtvalencia_app_icon from '@/assets/emtvalencia_app_icon.webp';
+import app_QR from '@/assets/qr_code_ios.webp';
 import { Icon } from '@/assets/icons/Icon';
 import { Button } from '../Button/Button';
+import 'swiper/swiper-bundle.css';
+import { Pagination } from 'swiper/modules';
 
 const cx = classNames.bind(theme);
 
 export const AppSection = () => {
   const { t } = useTranslation('apps');
+  const navigate = useNavigate();
 
   return (
     <>
@@ -20,81 +25,209 @@ export const AppSection = () => {
         <h2 className={cn('label-button', 'text__color--onbrand', 'center-text')}>{t('apps.downloadApps')}</h2>
       </div>
 
-      <h3 className={cn('display-large', 'text__color--brand')}>{t('apps.claim')}</h3>
+      <h3 className={cn('display-large', 'text__color--brand', cx('apps__header'))}>{t('apps.claim')}</h3>
 
-      <AppCard>
-        <img className={cx('apps__image')} src={emticket_app_icon} alt={t('apps.emticket.altText')} />
+      <Swiper
+        pagination={{ dynamicBullets: true }}
+        modules={[Pagination]}
+        className={cx('mySwiper')}
+        slidesPerView="auto"
+        spaceBetween={20}
+        breakpoints={{
+          500: {
+            slidesPerView: 'auto',
+            spaceBetween: 40,
+            centeredSlides: true,
+          },
+        }}
+      >
+        <SwiperSlide className={cx('slide-auto')}>
+          <AppCard>
+            <img className={cx('apps__image')} src={emticket_app_icon} alt={t('apps.emticket.altText')} />
 
-        <div className={cx('apps__content')}>
-          <h4 className={cn('display-cards', 'center-text', 'text__color--onbrand')}>{t('apps.emticket.title')}</h4>
+            <div className={cx('apps__content')}>
+              <h4 className={cn('display-cards', 'center-text', 'text__color--onbrand')}>{t('apps.emticket.title')}</h4>
 
-          <div className={cx('apps__info')}>
-            <span className={cx('apps__bulletpoint')}>
-              <Icon icon="AsteriskLine" color="onbrand" size="small" />
-              <p className={cn('paragraph-small', 'text__color--onbrand')}>
-                {t('apps.emticket.features.easyPurchase')}
-              </p>
-            </span>
+              <div className={cx('apps__info')}>
+                <span className={cx('apps__bulletpoint')}>
+                  <Icon icon="AsteriskLine" color="onbrand" size="small" />
+                  <p className={cn('paragraph-small', 'text__color--onbrand', 'text-align--left')}>
+                    {t('apps.emticket.features.easyPurchase')}
+                  </p>
+                </span>
 
-            <span className={cx('apps__bulletpoint')}>
-              <Icon icon="AsteriskLine" color="onbrand" size="small" />
-              <p className={cn('paragraph-small', 'text__color--onbrand')}>
-                {t('apps.emticket.features.virtualWallet')}
-              </p>
-            </span>
+                <span className={cx('apps__bulletpoint')}>
+                  <Icon icon="AsteriskLine" color="onbrand" size="small" />
+                  <p className={cn('paragraph-small', 'text__color--onbrand', 'text-align--left')}>
+                    {t('apps.emticket.features.virtualWallet')}
+                  </p>
+                </span>
 
-            <span className={cx('apps__bulletpoint')}>
-              <Icon icon="AsteriskLine" color="onbrand" size="small" />
-              <p className={cn('paragraph-small', 'text__color--onbrand')}>
-                {t('apps.emticket.features.advancePurchase')}
-              </p>
-            </span>
+                <span className={cx('apps__bulletpoint')}>
+                  <Icon icon="AsteriskLine" color="onbrand" size="small" />
+                  <p className={cn('paragraph-small', 'text__color--onbrand', 'text-align--left')}>
+                    {t('apps.emticket.features.advancePurchase')}
+                  </p>
+                </span>
+              </div>
+            </div>
+
+            <div className={cx('apps__button--hidden')}>
+              <Button
+                type="button"
+                color="onbrand"
+                fullWidth={true}
+                style="filled"
+                onClick={() => navigate('/downloadApp')}
+              >
+                {t('apps.downloadButton')}
+              </Button>
+            </div>
+          </AppCard>
+        </SwiperSlide>
+
+        <SwiperSlide className={cx('slide-auto')}>
+          <AppCard>
+            <img className={cx('apps__image')} src={emtvalencia_app_icon} alt={t('apps.emtvalencia.altText')} />
+
+            <div className={cx('apps__content')}>
+              <h4 className={cn('display-cards', 'center-text', 'text__color--onbrand')}>
+                {t('apps.emtvalencia.title')}
+              </h4>
+
+              <div className={cx('apps__info')}>
+                <span className={cx('apps__bulletpoint')}>
+                  <Icon icon="AsteriskLine" color="onbrand" size="small" />
+                  <p className={cn('paragraph-small', 'text__color--onbrand', 'text-align--left')}>
+                    {t('apps.emtvalencia.features.checkTimes')}
+                  </p>
+                </span>
+
+                <span className={cx('apps__bulletpoint')}>
+                  <Icon icon="AsteriskLine" color="onbrand" size="small" />
+                  <p className={cn('paragraph-small', 'text__color--onbrand', 'text-align--left')}>
+                    {t('apps.emtvalencia.features.saveStops')}
+                  </p>
+                </span>
+
+                <span className={cx('apps__bulletpoint')}>
+                  <Icon icon="AsteriskLine" color="onbrand" size="small" />
+                  <p className={cn('paragraph-small', 'text__color--onbrand', 'text-align--left')}>
+                    {t('apps.emtvalencia.features.rechargeCards')}
+                  </p>
+                </span>
+              </div>
+            </div>
+
+            <div className={cx('apps__button--hidden')}>
+              <Button
+                type="button"
+                color="onbrand"
+                fullWidth={true}
+                style="filled"
+                onClick={() => navigate('/downloadApp')}
+              >
+                {t('apps.downloadButton')}
+              </Button>
+            </div>
+          </AppCard>
+        </SwiperSlide>
+      </Swiper>
+
+      <div className={cx('apps__container-desktop')}>
+        <AppCard>
+          <img className={cx('apps__image')} src={emticket_app_icon} alt={t('apps.emticket.altText')} />
+
+          <div className={cx('apps__content')}>
+            <h4 className={cn('display-cards', 'center-text', 'text__color--onbrand')}>{t('apps.emticket.title')}</h4>
+
+            <div className={cx('apps__info')}>
+              <span className={cx('apps__bulletpoint')}>
+                <Icon icon="AsteriskLine" color="onbrand" size="small" />
+                <p className={cn('paragraph-small', 'text__color--onbrand', 'text-align--left')}>
+                  {t('apps.emticket.features.easyPurchase')}
+                </p>
+              </span>
+
+              <span className={cx('apps__bulletpoint')}>
+                <Icon icon="AsteriskLine" color="onbrand" size="small" />
+                <p className={cn('paragraph-small', 'text__color--onbrand', 'text-align--left')}>
+                  {t('apps.emticket.features.virtualWallet')}
+                </p>
+              </span>
+
+              <span className={cx('apps__bulletpoint')}>
+                <Icon icon="AsteriskLine" color="onbrand" size="small" />
+                <p className={cn('paragraph-small', 'text__color--onbrand', 'text-align--left')}>
+                  {t('apps.emticket.features.advancePurchase')}
+                </p>
+              </span>
+            </div>
           </div>
-        </div>
 
-        <div className={cx('apps__button--hidden')}>
-          <Button type="button" color="onbrand" fullWidth={true} style="filled" onClick={() => console.log('enlace')}>
-            {t('apps.downloadButton')}
-          </Button>
-        </div>
-      </AppCard>
-
-      <AppCard>
-        <img className={cx('apps__image')} src={emtvalencia_app_icon} alt={t('apps.emtvalencia.altText')} />
-
-        <div className={cx('apps__content')}>
-          <h4 className={cn('display-cards', 'center-text', 'text__color--onbrand')}>{t('apps.emtvalencia.title')}</h4>
-
-          <div className={cx('apps__info')}>
-            <span className={cx('apps__bulletpoint')}>
-              <Icon icon="AsteriskLine" color="onbrand" size="small" />
-              <p className={cn('paragraph-small', 'text__color--onbrand')}>
-                {t('apps.emtvalencia.features.checkTimes')}
-              </p>
-            </span>
-
-            <span className={cx('apps__bulletpoint')}>
-              <Icon icon="AsteriskLine" color="onbrand" size="small" />
-              <p className={cn('paragraph-small', 'text__color--onbrand')}>
-                {t('apps.emtvalencia.features.saveStops')}
-              </p>
-            </span>
-
-            <span className={cx('apps__bulletpoint')}>
-              <Icon icon="AsteriskLine" color="onbrand" size="small" />
-              <p className={cn('paragraph-small', 'text__color--onbrand')}>
-                {t('apps.emtvalencia.features.rechargeCards')}
-              </p>
-            </span>
+          <div className={cx('apps__button--hidden')}>
+            <Button
+              type="button"
+              color="onbrand"
+              fullWidth={true}
+              style="filled"
+              onClick={() => navigate('/downloadApp')}
+            >
+              {t('apps.downloadButton')}
+            </Button>
           </div>
-        </div>
+        </AppCard>
 
-        <div className={cx('apps__button--hidden')}>
-          <Button type="button" color="onbrand" fullWidth={true} style="filled" onClick={() => console.log('enlace')}>
-            {t('apps.downloadButton')}
-          </Button>
-        </div>
-      </AppCard>
+        <AppCard>
+          <img className={cx('apps__image')} src={emtvalencia_app_icon} alt={t('apps.emtvalencia.altText')} />
+
+          <div className={cx('apps__content')}>
+            <h4 className={cn('display-cards', 'center-text', 'text__color--onbrand')}>
+              {t('apps.emtvalencia.title')}
+            </h4>
+
+            <div className={cx('apps__info')}>
+              <span className={cx('apps__bulletpoint')}>
+                <Icon icon="AsteriskLine" color="onbrand" size="small" />
+                <p className={cn('paragraph-small', 'text__color--onbrand', 'text-align--left')}>
+                  {t('apps.emtvalencia.features.checkTimes')}
+                </p>
+              </span>
+
+              <span className={cx('apps__bulletpoint')}>
+                <Icon icon="AsteriskLine" color="onbrand" size="small" />
+                <p className={cn('paragraph-small', 'text__color--onbrand', 'text-align--left')}>
+                  {t('apps.emtvalencia.features.saveStops')}
+                </p>
+              </span>
+
+              <span className={cx('apps__bulletpoint')}>
+                <Icon icon="AsteriskLine" color="onbrand" size="small" />
+                <p className={cn('paragraph-small', 'text__color--onbrand', 'text-align--left')}>
+                  {t('apps.emtvalencia.features.rechargeCards')}
+                </p>
+              </span>
+            </div>
+          </div>
+
+          <div className={cx('apps__button--hidden')}>
+            <Button
+              type="button"
+              color="onbrand"
+              fullWidth={true}
+              style="filled"
+              onClick={() => navigate('/downloadApp')}
+            >
+              {t('apps.downloadButton')}
+            </Button>
+          </div>
+        </AppCard>
+      </div>
+
+      <div className={cx('apps__QR')}>
+        <h2 className={cn('display-medium', 'text__color--onprimary', 'center-text')}>{t('apps.downloadApps')}</h2>
+        <img className={cx('apps__image--QR')} src={app_QR} alt={t('apps.altQR')} />
+      </div>
     </>
   );
 };
