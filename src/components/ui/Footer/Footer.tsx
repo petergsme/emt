1ac40728b9645ad1d055756a6a43bbeeec64e5c2
Pedro Garcia <garciasan@esat-alumni.com>
2025-06-18 00:27:03 +0200

    footer desktop finished, polished

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -1,112 +1,192 @@
 import { useState, useEffect } from 'react';
-import './Footer.scss';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import classNames from 'classnames/bind';
+import cn from 'classnames';
+import styles from './footer.module.scss';
 import { FooterMobile } from './FooterMobile/FooterMobile';
+import { Icon } from '@/assets/icons/Icon';
 import emtQR from '@/assets/emt-qr.svg';
 import generalitatLogo from '../../../assets/generalitat-logo.svg';
 import fgvLogo from '../../../assets/fgv-logo.svg';
 import tramLogo from '../../../assets/tram-logo.svg';
-import socialIcons from '../../../assets/icons/assets/social-icons.svg';
+import { useLanguageSwitcher } from '@/hooks/useLanguageSwitcher';
+
+const cx = classNames.bind(styles);
 
 export const Footer = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 700);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
+
+  const { t } = useTranslation('footer');
+  const { changeLanguage, isActive } = useLanguageSwitcher();
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 700);
+      setIsMobile(window.innerWidth <= 1024);
     };
 
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   if (isMobile) return <FooterMobile />;
 
   return (
-    <footer className="footer">
-      <div className="footer__main">
-        <a className="footer__title">Volver arriba</a>
-        <div className="footer__content">
-          <div className="footer__map">
-            <a className="footer__h3">Mapa</a>
+    <footer className={cx('footer')}>
+      <div className={cx('footer__main')}>
+        <div className={cx('footer__content')}>
+          <div>
+            <Link to="/map" className={cn('display-small', cx('footer__link'))}>
+              {t('navigation.map')}
+            </Link>
           </div>
-          <div className="footer__fees">
-            <div className="footer__fees-content">
-              <a href="/tarifas" className="footer__h3">
-                Tarifas y Títulos
-              </a>
-              <a href="/tarjetas" className="footer__p">
-                Tarjetas
-              </a>
-              <a className="footer__p">Consultas y recargas</a>
-              <a className="footer__p">Registra tu tarjeta</a>
+
+          <div className={cx('footer__section')}>
+            <div className={cx('footer__section-content')}>
+              <h3 className={cn('display-small')}>{t('navigation.titlesAndRates.main')}</h3>
+              <Link to="/mobilis-cards" className={cn('paragraph-small', cx('footer__link'))}>
+                {t('navigation.titlesAndRates.cards')}
+              </Link>
+              <Link to="/mobilis-cards#recharge" className={cn('paragraph-small', cx('footer__link'))}>
+                {t('navigation.titlesAndRates.queries')}
+              </Link>
+              <Link to="/mobilis-cards#register" className={cn('paragraph-small', cx('footer__link'))}>
+                {t('navigation.titlesAndRates.register')}
+              </Link>
             </div>
-            <div className="footer__phone-content">
-              <a className="footer__h3">Atención Telefónica</a>
-              <a className="footer__p">96 315 85 15</a>
-              <a className="footer__p">
-                L-V de 8:00 a 21:00
+
+            <div className={cx('footer__section-content')}>
+              <h3 className={cn('display-small')}>{t('navigation.phone.main')}</h3>
+              <a href="tel:963158515" className={cn('questions-small', cx('footer__link'))}>
+                {t('navigation.phone.number')}
+              </a>
+              <p className={cn('paragraph-small', cx('footer__text'))}>
+                {t('navigation.phone.schedule.weekdays')}
                 <br />
-                Sábados de 9:00 a 14:00
-              </a>
-            </div>
-          </div>
-          <div className="footer__help">
-            <div className="footer__help-content">
-              <a className="footer__h3">Ayuda</a>
-              <a className="footer__p">Uso del bus y normativas</a>
-              <a className="footer__p">Preguntas frecuentes</a>
-            </div>
-            <div className="footer__legal-content">
-              <a className="footer__h3">Legal</a>
-              <a className="footer__p">Política de privacidad</a>
-              <a className="footer__p">Cookies</a>
+                {t('navigation.phone.schedule.saturday')}
+              </p>
             </div>
           </div>
-          <div className="footer__attention">
-            <div className="footer__attention-content">
-              <a className="footer__h3">Atención Presencial</a>
-              <a className="footer__p">
-                C/ Mendoza 5, 46001, Valencia
-                <br />
-                L-V de 9:00 a 19:30
-              </a>
-              <a className="footer__p">
-                UPV, Pl. del Ágora
-                <br />
-                L-V de 8:00 a 15:30
-              </a>
-              <a className="footer__p">
-                Estación de metro-colón
-                <br />
-                L-V de 9:00 a 19:00
-              </a>
-              <a className="footer__p">
-                La atencion presencial requiere cita previa
-                <br />
-                <span>Pedir cita previa →</span>
-              </a>
+
+          <div className={cx('footer__section')}>
+            <div className={cx('footer__section-content')}>
+              <h3 className={cn('display-small')}>{t('navigation.help.main')}</h3>
+              <Link to="/help/rules-guides" className={cn('paragraph-small', cx('footer__link'))}>
+                {t('navigation.help.regulations')}
+              </Link>
+              <Link to="/faq" className={cn('paragraph-small', cx('footer__link'))}>
+                {t('navigation.help.faq')}
+              </Link>
+            </div>
+
+            <div className={cx('footer__section-content')}>
+              <h3 className={cn('display-small')}>{t('navigation.legal.main')}</h3>
+              <Link to="/privacy" className={cn('paragraph-small', cx('footer__link'))}>
+                {t('navigation.legal.privacy')}
+              </Link>
+              <Link to="/cookies" className={cn('paragraph-small', cx('footer__link'))}>
+                {t('navigation.legal.cookies')}
+              </Link>
             </div>
           </div>
-          <div className="footer__qr">
-            <div className="footer__qr-content">
-              <img className="footer_qrcode" src={emtQR} alt="EMT QR" />
-              <a className="footer__h2">Descarga la App</a>
+
+          <div className={cx('footer__section')}>
+            <div className={cx('footer__section-content')}>
+              <h3 className={cn('display-small')}>{t('navigation.inPerson.main')}</h3>
+              <p className={cn('paragraph-small', cx('footer__text'))}>
+                <span className={cn('questions-small')}>{t('navigation.inPerson.mendoza.address')}</span>
+                <br />
+                {t('navigation.inPerson.mendoza.schedule')}
+              </p>
+              <p className={cn('paragraph-small', cx('footer__text'))}>
+                <span className={cn('questions-small')}>{t('navigation.inPerson.upv.address')}</span>
+                <br />
+                {t('navigation.inPerson.upv.schedule')}
+              </p>
+              <p className={cn('paragraph-small', cx('footer__text'))}>
+                <span className={cn('questions-small')}>{t('navigation.inPerson.colon.address')}</span>
+                <br />
+                {t('navigation.inPerson.colon.schedule')}
+              </p>
+              <p className={cn('paragraph-small', cx('footer__text'))}>
+                <span className={cn('paragraph-small')}>{t('navigation.inPerson.appointment.required')}</span>
+                <br />
+                <Link to="/appointment" className={cn('link-small')}>
+                  {t('navigation.inPerson.appointment.request')}
+                </Link>
+              </p>
             </div>
           </div>
+
+          <div className={cx('footer__section')}>
+            <div className={cx('footer__qr-content')}>
+              <img className={cx('footer__qr-image')} src={emtQR} alt={t('navigation.qr.alt')} />
+              <h3 className={cn('display-cards')}>{t('actions.downloadApp')}</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className={cx('footer__language-scrollup')}>
+          <div className={cx('footer__language-switcher')}>
+            <button
+              onClick={() => changeLanguage('es')}
+              className={cn('paragraph-small', cx('language-button', { 'language-button--active': isActive('es') }))}
+            >
+              {t('language.es')}
+            </button>
+            <button
+              onClick={() => changeLanguage('ca')}
+              className={cn('paragraph-small', cx('language-button', { 'language-button--active': isActive('ca') }))}
+            >
+              {t('language.val')}
+            </button>
+            <button
+              onClick={() => changeLanguage('en')}
+              className={cn('paragraph-small', cx('language-button', { 'language-button--active': isActive('en') }))}
+            >
+              {t('language.en')}
+            </button>
+          </div>
+          <button
+            type="button"
+            onClick={handleScrollToTop}
+            className={cn('link-small', 'text__color--onbrand', cx('footer__back-to-top'))}
+          >
+            {t('actions.backToTop')}
+          </button>
         </div>
       </div>
-      <div className="footer__end">
-        <div className="footer__end__collab-logos">
-          <img className="footer_generalitat" src={generalitatLogo} alt="logo generalitat valenciana" />
-          <img className="footer_fgv" src={fgvLogo} alt="logo fgv" />
-          <img className="footer_tram" src={tramLogo} alt="logo tram" />
+
+      <div className={cx('footer__bottom')}>
+        <div className={cx('footer__collab-logos')}>
+          <img className={cx('footer__logo')} src={generalitatLogo} alt={t('navigation.logos.generalitat')} />
+          <img className={cx('footer__logo')} src={fgvLogo} alt={t('navigation.logos.fgv')} />
+          <img className={cx('footer__logo')} src={tramLogo} alt={t('navigation.logos.tram')} />
         </div>
-        <div className="footer__end__media-icons">
-          <img className="footer_social" src={socialIcons} alt="social icons"></img>
+
+        <div className={cx('footer__social')}>
+          <a target="_blank" href="https://wa.me/34658674920">
+            <Icon icon="Whatsapp" color="onprimary-secondary" size="large" />
+          </a>
+          <a target="_blank" href="https://t.me/oacemtvalencia">
+            <Icon icon="Telegram" color="onprimary-secondary" size="large" />
+          </a>
+          <a target="_blank" href="https://www.facebook.com/emtvalencia/">
+            <Icon icon="Facebook" color="onprimary-secondary" size="large" />
+          </a>
+          <a target="_blank" href="https://twitter.com/emtvalencia">
+            <Icon icon="Twitter" color="onprimary-secondary" size="large" />
+          </a>
+          <a target="_blank" href="mailto:atencioalaciutadania@emtvalencia.es">
+            <Icon icon="Mail" color="onprimary-secondary" size="large" />
+          </a>
         </div>
-        <div className="footer__end__copyright">
-          <a>© Copyright EMT Valencia 2025.</a>
-        </div>
+
+        <p className={cn('paragraph-xxsmall', 'text__color--onprimary')}>{t('legal.copyright')}</p>
       </div>
     </footer>
   );
